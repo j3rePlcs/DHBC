d40b69bb07ee577035f1d923a3989ebf862894db
camilamorand251 <magalicamila251@gmail.com>
2022-10-07 21:06:16 -0500

    mod:validacionLogin.js

--- public/js/validacionLogin.js
+++ public/js/validacionLogin.js
@@ -1,4 +1,9 @@
 window.addEventListener("load", function() {
 
     let form = document.querySelector (".form")
+
+    form.addEventListener("submit", (e) => {  
+        let errores = [];
+        let campoUsuario = document.querySelector("input#name");
+    })
 })
