eb4465f158b1e777d57d3aebd98a945439849f13
camilamorand251 <magalicamila251@gmail.com>
2022-10-07 21:20:43 -0500

    mod:login.css:errores

--- public/js/validacionLogin.js
+++ public/js/validacionLogin.js
@@ -1,26 +1,28 @@
 window.addEventListener("load", function() {
 
-    let form = document.querySelector (".form")
+   let form = document.querySelector (".form")
 
     form.addEventListener("submit", (e) => {  
-    let errores = [];
+     let errores = [];
      let campoUsuario = document.querySelector("input#name");
      
-     if (campoUsuario.value.length < 4) {
+    if (campoUsuario.value.length < 4) {
       errores.push("El campo de Usuario debe completarse");
     }
-    let campoContraseña = document.querySelector("input#contra");
 
-     if (campoContraseña.value.length < 4){
+     let campoContraseña = document.querySelector("input#contra");
+
+    if (campoContraseña.value.length < 4){
       errores.push("El campo de Contraseña debe completarse");
-     }
-       if (errores.length > 0 ){
-        e.preventDefault ();
-  
-        let ulErrores = document.querySelector(".errores ul");
-        errores.forEach(error => {
-           ulErrores.innerHTML += `<li>${error}</li>`
+    }
+    if (errores.length > 0 ){
+      e.preventDefault ();
+
+      let ulErrores = document.querySelector(".errores ul");
+      
+      errores.forEach(error => {
+         ulErrores.innerHTML += `<li>${error}</li>`
         });
-     }
-   });
-  })
+    }
+  });
+})
