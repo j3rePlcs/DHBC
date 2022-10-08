644ab3ffe517af2a982f8c84c8fc49aea9f178bb
camilamorand251 <magalicamila251@gmail.com>
2022-10-07 21:12:44 -0500

    mod: validacionLogin.js; campoContraseña

--- public/js/validacionLogin.js
+++ public/js/validacionLogin.js
@@ -2,12 +2,17 @@ window.addEventListener("load", function() {
 
     let form = document.querySelector (".form")
 
-  form.addEventListener("submit", (e) => {  
-   let errores = [];
+    form.addEventListener("submit", (e) => {  
+    let errores = [];
      let campoUsuario = document.querySelector("input#name");
      
      if (campoUsuario.value.length < 4) {
       errores.push("El campo de Usuario debe completarse");
     }
+    let campoContraseña = document.querySelector("input#contra");
+
+     if (campoContraseña.value.length < 4){
+      errores.push("El campo de Contraseña debe completarse");
+     }
 })
 })
