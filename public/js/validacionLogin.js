5103c8d8b005e8f5d9838dc10e73fa3456150f08
camilamorand251 <magalicamila251@gmail.com>
2022-10-07 21:10:52 -0500

    mod: validacionLogin.js; campoUsuario

--- public/js/validacionLogin.js
+++ public/js/validacionLogin.js
@@ -2,8 +2,12 @@ window.addEventListener("load", function() {
 
     let form = document.querySelector (".form")
 
-    form.addEventListener("submit", (e) => {  
-        let errores = [];
-        let campoUsuario = document.querySelector("input#name");
-    })
+  form.addEventListener("submit", (e) => {  
+   let errores = [];
+     let campoUsuario = document.querySelector("input#name");
+     
+     if (campoUsuario.value.length < 4) {
+      errores.push("El campo de Usuario debe completarse");
+    }
+})
 })
