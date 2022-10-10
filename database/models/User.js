6ec3a64d6efa7db3168d449eca965d73648eaf57
camilamorand251 <magalicamila251@gmail.com>
2022-10-10 16:24:04 -0500

    add: sequelize, mod:package.json, package-lock.json

---
+++ database/models/User.js
@@ -0,0 +1,57 @@
+module.exports = function (sequelize, DataTypes) {
+    let name = "User";
+    let cols = {
+        "dateCreation": {
+            "type": DataTypes.DATE,
+            "allowNull": false
+        },
+        "name": {
+            "type": DataTypes.STRING(15),
+            "allowNull": false
+        },
+        "email": {
+            "type": DataTypes.STRING(45)
+        },
+        "passwordUser": {
+            "type": DataTypes.STRING(45),
+            "allowNull": false
+        },
+        "idStatusUser": {
+            "type": DataTypes.INTEGER
+        },
+        "idOrdely_turns": {
+            "type": DataTypes.INTEGER
+        }
+    };
+    let config = {
+        "tableName": "users",
+        "timestamps": false
+    };
+
+    let User = sequelize.define(name, cols, config);
+
+    User.associate = function (models) {
+
+        User.hasMany(models.Card, {
+            "as": "cards",
+            "foreignKey": "idUser"
+        });
+
+        User.belongsTo(models.StatusUser, {
+            "as": "status",
+            "foreignKey": "idStatusUser"
+        });
+
+        User.belongsTo(models.Delivery, {
+            "as": "ubication",
+            "foreignKey": "postalCode"
+        });
+
+        User.hasMany(models.Cart, {
+            "as": "user",
+            "foreignKey": "idUser"
+        });
+    };
+
+    return User
+}
