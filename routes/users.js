bdc70766ff99bef7e3e761b6d3b65669f6199c6f
camilamorand251 <magalicamila251@gmail.com>
2022-10-11 19:27:07 -0500

    add:models/Operation_type, mod:user.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const userController = require('../src/controllers/userController');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//editar
+router.get('/edit:id', userController.edit)
+
+router.get('/edit:id', userController.edit)
+
 
 module.exports = router;
