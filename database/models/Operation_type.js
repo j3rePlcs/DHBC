16ed2f4d2a38d9c32914ac0b1d2ced9d5eae7a42
camilamorand251 <magalicamila251@gmail.com>
2022-10-11 20:25:39 -0500

    mod:Operation_type

---
+++ database/models/Operation_type.js
@@ -0,0 +1,9 @@
+module.exports = function (sequelize, DataTypes) {
+    let name = "Operation_type"
+    let cols = {
+        "nameOperation": {
+            "type": DataTypes.STRING(10),
+            "allowNull": false
+        }
+    }
+}
