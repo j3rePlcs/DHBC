e631bcc85b4b335c2f874d7fcad0343b91147297
camilamorand251 <magalicamila251@gmail.com>
2022-10-11 22:52:02 -0500

    mod:Operation_type

--- database/models/Operation_type.js
+++ database/models/Operation_type.js
@@ -5,5 +5,21 @@ module.exports = function (sequelize, DataTypes) {
             "type": DataTypes.STRING(10),
             "allowNull": false
         }
-    }
+    };
+    let config = {
+        "tableName": "operation_type",
+        "timestamps": false
+    };
+    const Operation_type = sequelize.define(name, cols, config);
+
+    Operation_type.associate = function (models) {
+
+        Operation.hasMany(models.Product, {
+            "as": "operation_type",
+            "foreignKey": "idOperation_type"
+        });
+
+    };
+
+    return Operation_type
 }
